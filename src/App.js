e4f44ac4fc00cbd56a31cd4b1ab89e5e4ce9362f
Manoj Geddannavar <MANOJGEDDANNAVAR99@GMAIL.COM>
2024-12-11 22:56:18 +0530

    Initial commit with downloaded files

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [currentImage, setCurrentImage] = useState(null);
+
+  
+  const images = [
+    { name: "Image 1", src: "/images/IMG-20241211-WA0015.jpg" },
+    { name: "Image 2", src: "/images/IMG-20241211-WA0016.jpg" },
+    { name: "Image 3", src: "/images/IMG-20241211-WA0017.jpg" },
+    { name: "Image 4", src: "/images/IMG-20241211-WA0024.jpg" },
+  ];
+
+  const handleImageClick = (image) => {
+    setCurrentImage(image);
+  };
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Welcome to the Landing Page</h1>
+      </header>
+      <div className="content">
+        <aside className="links">
+          <ul>
+            {images.map((img, index) => (
+              <li key={index}>
+                <button onClick={() => handleImageClick(img.src)}>
+                  {img.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </aside>
+        <main className="image-display">
+          {currentImage ? (
+            <img src={currentImage} alt="Selected" />
+          ) : (
+            <p>Click on a link to view the image.</p>
+          )}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
